d92049576eb5f488f0f9247cf4842b44f4d033c8
choi hyun won <danboys@domfam.com>
2020-09-02 19:27:04 +0900

    1. 오른쪽 첫번째 레이아웃창 닫았을 때 기능은 정상적으로 작동하나 아래와 같이 오류가 발생하는 중
    2. 오른쪽 레이아웃창 가로형/세로형 버튼으로 전환 후 열려있던 레이아웃창을 모두 닫으면 그 후 새로운 레이아웃 창이 안열림.

--- js/layout/layoutsystem.js
+++ js/layout/layoutsystem.js
@@ -2,53 +2,67 @@ var config = {
     content: [{
         type: 'column',
         isClosable: false,
-        content: [
-        ]
+        content: []
     }]
 };
 var myLayout = new window.GoldenLayout(config, $('#container_system'));
 
 
-myLayout.on( 'stateChanged', function(a){
-    if (!a) { return false };
-    var p = a.origin.contentItems[0].parent;
-    // console.log(p)
-    for (i=0; i<p.contentItems.length; i++){
-        var h = p.contentItems[i].element.height();
-        // console.log(i)
-        console.log(h)
-        p.contentItems[i].element.find('iframe').height(h-20);
+myLayout.on( 'stateChanged', function(a) {
+    if (!a) {
+        return false
+    }
+    if (a.origin.contentItems.length > 0) {
+        var p = a.origin.contentItems[0].parent;
+        // console.log(p)
+        for (i = 0; i < p.contentItems.length; i++) {
+            var h = p.contentItems[i].element.height();
+            // console.log(i)
+            console.log(h);
+            p.contentItems[i].element.find('iframe').height(h - 20);
+        }
     }
 });
 
 myLayout.addMenuItem = function (title, id) {
-    var aPanel = myLayout.root.contentItems[0].contentItems;
-    var nSize = aPanel.length;
-    var newItemConfig = {
-        title: title,
-        type: 'component',
-        componentName: id,
-        componentState: {text: id}
-    };
-    if( nSize < 3) {
-        for (i = 0; i < nSize; i++ ) {
-            if (myLayout.root.contentItems[0].contentItems[i].isMaximised) {
-                myLayout.root.contentItems[0].contentItems[i].toggleMaximise();}
-        }
+    if (myLayout.root.contentItems.length === 0) {
+        var parentItemConfig = {
+            type: 'column',
+            content: []
+        };
+        myLayout.root.addChild(parentItemConfig);
+
+        var newItemConfig = {
+            title: title,
+            type: 'component',
+            componentName: id,
+            componentState: {text: id}
+        };
         myLayout.root.contentItems[0].addChild(newItemConfig);
-    } else if (nSize === 3) {
-        myLayout.root.contentItems[0].contentItems[0].addChild(newItemConfig);
+    } else {
+        var aPanel = myLayout.root.contentItems[0].contentItems;
+        var nSize = aPanel.length;
+        var newItemConfig = {
+            title: title,
+            type: 'component',
+            componentName: id,
+            componentState: {text: id}
+        };
+        if (nSize < 3) {
+            for (i = 0; i < nSize; i++) {
+                if (myLayout.root.contentItems[0].contentItems[i].isMaximised) {
+                    myLayout.root.contentItems[0].contentItems[i].toggleMaximise();
+                }
+            }
+            myLayout.root.contentItems[0].addChild(newItemConfig);
+        } else if (nSize === 3) {
+            myLayout.root.contentItems[0].contentItems[0].addChild(newItemConfig);
+        }
     }
+
 };
-// myLayout.on( 'stackCreated', function( stack ){
-//     var container = stack.contentItems[0].element;
-//     console.log(container.find('.lm_content'));
-//     container.find('.lm_content').text('hello!');
-// })
-
 myLayout.toggleRowCol = function() {
-
-    var oldElement = myLayout.root.contentItems[ 0 ],
+    var oldElement = myLayout.root.contentItems[0],
         newElement = myLayout.createContentItem({
             type: oldElement.isRow ? 'column' : 'row',
             content: []
@@ -56,7 +70,7 @@ myLayout.toggleRowCol = function() {
         i;
     newElement.isInitialised = true;
     for( i = 0; i < oldElement.contentItems.length; i++ ) {
-        newElement.addChild( oldElement.contentItems[ i ] );
+        newElement.addChild(oldElement.contentItems[i]);
     }
     myLayout.root.replaceChild( oldElement, newElement );
 };
